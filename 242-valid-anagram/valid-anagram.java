7a88b6fbe80c7f0895ba2e2cbbe78f437662f3e8
Pulkit0923 <2k23.csiot2311109@gmail.com>
2026-04-21 23:06:43 +0530

    Time: 4 ms (82.39%) | Memory: 46.6 MB (25.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,65 +1,39 @@
-//brute force
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-//       if(s.length()!= t.length()){
-//         return false;
-//       }
-//       else {                                              
-//       char[] a= s.toCharArray();
-//       char[] b= t.toCharArray();
-//       Arrays.sort(a);
-//       Arrays.sort(b);
-//       for(int i =0;i<a.length;i++){
-//         if(a[i]!=b[i]){
-//           return false;
-//         }
-        
-//       }
-//       return true;
-//       }
-//     }
-// }
-
-
-// as hashmap works on o[1] so  this is optimal approach
-
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //base case
+       // brute force approach  [ tc= O(n log n ) ,  sc = O(n)]
+
         if(s.length() != t.length()) return false;
+        char[] a = s.toCharArray(); // remember this this is a built in function in java  O(n)
+        char[] b = t.toCharArray();
 
-        HashMap<Character,Integer> smap = new HashMap<>();
-        HashMap<Character,Integer> tmap = new HashMap<>();
+        Arrays.sort(a);   // O(log n)
+        Arrays.sort(b);
 
-        //filling hashmap 1 and then 2 
-
-        for(int i = 0; i<s.length();i++){
-            if(smap.containsKey(s.charAt(i))){  //checks for each index in string if it is present in map or not
-                int freq = smap.get(s.charAt(i));  // taking the value of that key(how many times it came)
-                smap.put(s.charAt(i),freq+1);      // if it already there then frequency++
-            }
-            else{
-                smap.put(s.charAt(i),1);  // else put 1 in place of value
-            }
+        for(int i = 0 ; i< s.length(); i++){  //O(n)
+            if(  a[i] != b[i]) return false;
         }
+        return true;
 
-         for(int i = 0; i<t.length();i++){
-            if(tmap.containsKey(t.charAt(i))){
-                int freq = tmap.get(t.charAt(i));
-                tmap.put(t.charAt(i),freq+1);
-            }
-            else{
-                tmap.put(t.charAt(i),1);
-            }
-        }
 
-        for(char ch: smap.keySet()){
-            if(!tmap.containsKey(ch) || !smap.get(ch).equals(tmap.get(ch))){
-    return false;
-}
-            
-        }
-     return true;   
+
+    // optimal approach (using hashmap)
+
+    // if(s.length() != t.length()) return false;
+    // HashMap <Character,Integer> map = new HashMap <>();
+
+    // for(int i = 0 ; i< s.length(); i++){
+    //     char ch = s.charAt(i);
+    //     char cn = t.charAt(i);
+    //     int freq = map.get(ch);
+    //     // int freq1 = map.get(cn);
+    // map.put(ch,freq+1);
+    // map.put(cn,freq-1);
+
+    // }
+    // for(int ele : map){
+    //     if(map.get(ele) != 0) return false;
+    // }
+    // return true;
+
     }
 }
