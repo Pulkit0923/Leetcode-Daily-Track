ada313103009bb99fe99415d3d6f12e6597a2e2c
Pulkit0923 <2k23.csiot2311109@gmail.com>
2026-06-07 23:34:12 +0530

    Time: 5 ms (46.19%) | Memory: 46.5 MB (96.88%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,21 +1,42 @@
-// this is brute sollution (array) 
+// // this is brute solutuin(array)
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int [] ans = new int[2];
+//         for(int i =0;i<nums.length;i++){
+//             boolean flag =false;
+//             for(int j = i+1;j<nums.length;j++){
+//                 if( nums[i] + nums[j]== target){
+//                     ans[0]=i;
+//                     ans[1]=j;
+//                     flag = true;
+//                     break;
+//                 }
+//             }
+//             if(flag== true){
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+// optimal solution(HashMap)
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int [] ans = new int[2];
-        for(int i =0;i<nums.length;i++){
-            boolean flag =false;
-            for(int j = i+1;j<nums.length;j++){
-                if( nums[i] + nums[j]== target){
-                    ans[0]=i;
-                    ans[1]=j;
-                    flag = true;
-                    break;
-                }
-            }
-            if(flag== true){
-                break;
-            }
+        HashMap<Integer,Integer> smap = new HashMap<>();
+        int[] ans = {-1,-1};
+        for(int i = 0 ;i<nums.length;i++){
+        int remaining = target - nums[i];
+        if(smap.containsKey(remaining)){
+            ans[0]= i;
+            ans[1]= smap.get(remaining);
         }
-        return ans;
+        smap.put(nums[i],i);
     }
+    return ans;
+}
 }
